ea384955d329fdb766d676e78f1ea918b8a121aa
Marko Pekica <markpeko@gmail.com>
2024-12-16 14:40:23 +0100

    Added route for fetching a single product by ID

--- src/main/kotlin/webshop/models/Product.kt
+++ src/main/kotlin/webshop/models/Product.kt
@@ -2,7 +2,7 @@ package webshop.models
 
 import kotlinx.serialization.Serializable
 
-@Serializable   // allows the class to automatically be converted to/from JSON by Kotlinx Serialization
+@Serializable
 data class Product(
     val id: Int,
     val name: String,
--- src/main/kotlin/webshop/routes/ProductRoutes.kt
+++ src/main/kotlin/webshop/routes/ProductRoutes.kt
@@ -14,4 +14,21 @@ fun Route.productRoutes() {
         )
         call.respond(products)
     }
+
+    get("/products/{id}") {
+        val products = listOf(
+            Product(1, "Laptop", 533.0),
+            Product(2, "Headphones", 60.0),
+            Product(3, "Lunch", 15.0)
+        )
+        val id = call.parameters["id"]?.toIntOrNull()
+        val product = products.find { it.id == id }
+
+        if (product != null) {
+            call.respond(product)
+        } else {
+            call.respondText("Product not found", status = io.ktor.http.HttpStatusCode.NotFound)
+        }
+    }
+
 }
